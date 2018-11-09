e4876834300e398128bce4af3d28b8bec9edc4cd
maollm <maollm@126.com>
2018-11-09 22:33:16 +0800

    add database

--- tango/rango/models.py
+++ tango/rango/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, True)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
